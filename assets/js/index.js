035ec9b992febe42e9ebd12715312db5220790c4
Jack Carmichael <41242785+carmichaeljr@users.noreply.github.com>
2020-10-16 20:22:55 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
-JSC.Chart('chartDiv', {});
+JSC.Chart('chartDiv', {
+   type: 'column',
+   series: [
+      {
+         points: [
+            {x: 'Squat', y: 365},
+            {x: 'Bench', y: 240},
+            {x: 'Deadlift', y: 425}
+         ]
+      }
+   ]
+});
