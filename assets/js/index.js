7ae941868d00a8f7539cec1407077a0c6d3fd867
Jack Carmichael <41242785+carmichaeljr@users.noreply.github.com>
2020-10-23 09:13:42 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -201,18 +201,18 @@ JSC.Grid("gridDiv", {
   grid = g;
 });
 
-function openPage(pageName,elmnt,color) {
+function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
+  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 // Get the element with id="defaultOpen" and click on it
