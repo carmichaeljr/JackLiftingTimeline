25563cbf2e1a9b5573800179161297fb3596e76f
Jack Carmichael <41242785+carmichaeljr@users.noreply.github.com>
2021-04-24 19:13:39 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -305,23 +305,3 @@ function openTab(evt, tabName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
-
-/*toggle between hiding and showing the dropdown content */
-function userSelect() {
-  document.getElementById("userDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
